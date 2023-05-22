a70843443ffbb2d7790487409521f97fac376d84
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-22 21:30:45 +0300

    Navbar logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,20 @@
-import React from 'react'
+// import React , {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  // const [] = useState();
   return (
-    <div>Navbar</div>
+   <>
+   <nav className='navbar'>
+    <div className='navbar-container'>
+    <Link to = "/" className = "navbar-logo">
+      TRVL  <i className='fab fa-typo3' />
+    </Link>
+
+    </div>
+
+   </nav>
+   </>
   )
 }
 
