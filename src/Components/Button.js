a7c98bdcbb221cac19f81c92d4b2bb2dd18a3175
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-25 17:38:00 +0300

    Button.js

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Button.css';
 import { Link } from 'react-router-dom';
 
+const STYLES = ['btn--primary' , 'btn--outline']
+const SIZES = ['btn--medium' , 'btn-large']
+
 export const Button = ({
   children, 
   type, 
@@ -9,7 +12,20 @@ export const Button = ({
    buttonStyle, 
    buttonSize
   }) => {
-    checkButtonStyle = STYLES.includes(buttonStyle);
- 
-}
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES [0]
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES [0]
+
+    return (
+      <Link to = '/sign-up' className='btn-mobile'>
+        <button className={`btn ${checkButtonStyle}, ${checkButtonSize}`} 
+        onClick = {onClick}
+        type = {type}>
+          {children}
+        </button>
+      </Link>
+    )
+
+    
+};
 
